cd6925eae723efb57416b5988e8631152378e7ff
INTMAX-jpg <1879553812@qq.com>
2026-06-05 21:15:47 +0800

    Use note font in footer

--- js/blog-interactions.js
+++ js/blog-interactions.js
@@ -47,7 +47,7 @@ function markGalleryNoteIntroSeen() {
   } catch (error) {}
 }
 
-const galleryNoteText = "\u6444\u5f71<br>\u5c31\u50cf\u662f\u4ece\u8fd9\u4e16\u754c\u88c1\u4e0b\u4e86\u4e00\u5e27<br>\u5e26\u7ed9\u5239\u90a3\u7a7f\u8d8a\u65f6\u7a7a\u7684\u529b\u91cf~<br><span class=\"gallery-note-signature\">--Zixi</span>";
+const galleryNoteText = "\u6444\u5f71<br>\u5c31\u50cf\u662f\u4ece\u8fd9\u4e16\u754c\u88c1\u4e0b\u4e86\u4e00\u5e27<br>\u5e26\u7ed9\u90a3\u5239\u90a3\u7a7f\u8d8a\u65f6\u7a7a\u7684\u529b\u91cf~<br><span class=\"gallery-note-signature\">--Zixi</span>";
 
 function removeGalleryNoteButton() {
   document.querySelectorAll(".gallery-note-reopen").forEach((element) => element.remove());
