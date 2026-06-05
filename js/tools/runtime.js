9ef38d4692247c6a3d2e8154975b6c9e1d880e2e
INTMAX-jpg <1879553812@qq.com>
2026-06-05 11:22:05 +0800

    Add next birthday countdown

--- js/tools/runtime.js
+++ js/tools/runtime.js
@@ -1,3 +1,7 @@
+const BIRTH_YEAR = 2005;
+const BIRTH_MONTH = 6;
+const BIRTH_DAY = 29;
+const BIRTH_HOUR = 5;
 const BIRTHDAY_100 = "2105/6/29 05:00:00";
 
 const splitDuration = (milliseconds) => {
@@ -16,11 +20,34 @@ const setRuntimeValue = (id, value) => {
   if (element) element.innerHTML = value;
 };
 
+const createBirthdayDate = (year) => new Date(year, BIRTH_MONTH - 1, BIRTH_DAY, BIRTH_HOUR, 0, 0);
+
+const formatBirthdayDate = (date) => `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+const getNextBirthday = (now) => {
+  let birthday = createBirthdayDate(now.getFullYear());
+  if (birthday.getTime() <= now.getTime()) {
+    birthday = createBirthdayDate(now.getFullYear() + 1);
+  }
+
+  return {
+    age: birthday.getFullYear() - BIRTH_YEAR,
+    date: birthday,
+  };
+};
+
 const renderDuration = (prefix, duration) => {
   setRuntimeValue(`${prefix}_days`, duration.days);
   setRuntimeValue(`${prefix}_hours`, duration.hours);
   setRuntimeValue(`${prefix}_minutes`, duration.minutes);
   setRuntimeValue(`${prefix}_seconds`, duration.seconds);
+};
+
+const renderNextBirthday = (now) => {
+  const nextBirthday = getNextBirthday(now);
+  setRuntimeValue("next_birthday_age", nextBirthday.age);
+  setRuntimeValue("next_birthday_date", formatBirthdayDate(nextBirthday.date));
+  renderDuration("next_birthday", splitDuration(nextBirthday.date.getTime() - now.getTime()));
 };
 
 const footerRuntime = () => {
@@ -31,6 +58,7 @@ const footerRuntime = () => {
   const birthday100 = new Date(BIRTHDAY_100);
 
   renderDuration("runtime", splitDuration(now.getTime() - startTime.getTime()));
+  renderNextBirthday(now);
   renderDuration("birthday", splitDuration(birthday100.getTime() - now.getTime()));
 };
 
